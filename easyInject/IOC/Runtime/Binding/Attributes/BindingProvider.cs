fd71be54239ca8ba111b6e758239172627dd7a75
Erick Najjar <enajjar@gazeus.com>
2018-07-25 18:36:14 -0300

    added the runtime inside the main project

---
+++ easyInject/IOC/Runtime/Binding/Attributes/BindingProvider.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Collections.Generic;
+
+
+
+namespace EasyInject.Engine.Runtime
+{
+	public class BindingProviderAttribute : System.Attribute
+	{
+		public object Name = IOC.InnerBindingNames.Empty;
+		public int DependencyCount = 0;
+		public object[] DependencieNames = new object[0];
+	}
+
+
+}
---
+++ easyInject/IOC/Runtime/Binding/IBindingProvider.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace EasyInject.Engine.Runtime
+{
+	public interface IBindingProvider 
+	{
+		IList<IBindingProvider> Dependencies{get;}
+	}
+
+
+}
